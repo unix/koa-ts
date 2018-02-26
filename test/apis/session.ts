8c73293adeb09b2dddf892c37efc60d895e31860
WittBulter <nanazuimeng123@gmail.com>
2018-02-26 17:15:05 +0800

    build(pkg): improve chai types

--- test/apis/session.ts
+++ test/apis/session.ts
@@ -1,14 +1,14 @@
 import * as Utils from '../utils.nyc'
 import { expect } from 'chai'
 
-describe('routers: session', () => {
+describe('routers: session', (): void => {
   
-  before(async () => {
+  before(async(): Promise<void> => {
     this.server = await Utils.app()
   })
   
-  it('should create a session', async() => {
-    const res = await this.server.get('/api/v1/sessions.json')
+  it('should create a session', async(): Promise<void> => {
+    const res: ChaiHttp.Response = await this.server.get('/api/v1/sessions.json')
     expect(res.status).equal(200)
   })
   
