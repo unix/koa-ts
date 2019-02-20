1958b06b37995db9724dbbb0cdff0d6d27dcfe20
WittBulter <nanazuimeng123@gmail.com>
2019-02-20 15:15:08 +0800

    chore: clearup

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,6 @@ import * as Koa from 'koa'
 import * as kcors from 'kcors'
 import * as logger from 'koa-logger'
 import * as bodyParser from 'koa-bodyparser'
-import { KoaCustomResponse } from 'koa-custom-response'
 import { Environment } from './environments'
 
 
@@ -12,8 +11,6 @@ export const useMiddlewares = <T extends Koa>(app: T): T => {
   Environment.identity !== 'test' && app.use(logger())
   
   app.use(bodyParser())
-  
-  app.use(KoaCustomResponse())
   
   return app
 }
