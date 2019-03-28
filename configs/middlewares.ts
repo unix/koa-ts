2dfeebaf50e66b805dafb8003e12cf1e7bc52941
WittBulter <nanazuimeng123@gmail.com>
2019-03-28 11:07:56 +0800

    fix: upgrade build script

--- configs/middlewares.ts
+++ configs/middlewares.ts
@@ -1,11 +1,12 @@
 import * as Koa from 'koa'
-import * as kcors from 'kcors'
+import * as kcors from '@koa/cors'
 import * as logger from 'koa-logger'
 import * as bodyParser from 'koa-bodyparser'
 import { Environment } from './environments'
 
 
 export const useMiddlewares = <T extends Koa>(app: T): T => {
+  // cors options: https://github.com/koajs/cors#corsoptions
   app.use(kcors())
   
   Environment.identity !== 'test' && app.use(logger())
