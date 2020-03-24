c34d9f3ba5d390fff65fb9df01ecc666a706193b
Hayden Young <hello@itshayden.dev>
2020-03-24 10:00:49 +0000

    refactor(database): Use ormconfig over a connection file

--- configs/application.ts
+++ configs/application.ts
@@ -1,23 +1,19 @@
 import 'reflect-metadata'
+import './connection'
 import * as Koa from 'koa'
 import { Container } from 'typedi'
-import { useMongoDB } from './customs'
 import { routingConfigs } from './routing.options'
 import { useMiddlewares } from './koa.middlewares'
 import { useKoaServer, useContainer } from 'routing-controllers'
-if (useMongoDB) {
-  require('./connection')
-}
 
-export const createServer = async(): Promise<Koa> => {
-  
+export const createServer = async (): Promise<Koa> => {
   const koa: Koa = new Koa()
-  
+
   useMiddlewares(koa)
-  
+
   const app: Koa = useKoaServer<Koa>(koa, routingConfigs)
-  
+
   useContainer(Container)
-  
+
   return app
 }
--- configs/connection.ts
+++ configs/connection.ts
@@ -1,26 +1,11 @@
-import * as entities from 'entities'
 import * as bootstrap from './bootstrap'
 import { print } from './utils'
-import { Environment } from './environments'
 import { createConnection } from 'typeorm'
-const mongo = Environment.mongo
 
+const mongoConnect = createConnection().then(() =>
+  print.log('database connected.')
+)
 
-const mongoConnect = createConnection({
-  type: 'mongodb',
-  host: mongo.MONGODB_HOST,
-  port: mongo.MONGODB_PORT,
-  username: mongo.MONGODB_USER,
-  database: mongo.MONGODB_DATABASE,
-  password: mongo.MONGODB_PASS,
-  useNewUrlParser: true,
-  entities: Object.keys(entities).map(name => entities[name]),
-})
-.then(() => print.log('connected mongodb.'))
-
-
-Promise.all([
-  mongoConnect,
-])
-.then(bootstrap.connected)
-.catch(error => console.log(error))
+Promise.all([mongoConnect])
+  .then(bootstrap.connected)
+  .catch(error => console.log(error))
